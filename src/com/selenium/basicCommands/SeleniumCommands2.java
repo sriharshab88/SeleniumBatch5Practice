198fcb6ee3d804928c27e4226bd9d977e32a2b10
Harsha <teamindia#456>
2018-10-19 08:26:57 +0530

    [Harsha] Added today's scripts related to core commands to repo

--- src/com/selenium/basicCommands/SeleniumCommands2.java
+++ src/com/selenium/basicCommands/SeleniumCommands2.java
@@ -8,17 +8,18 @@ import java.io.File;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.*;
-import org.testng.Assert;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
-public class SeleniumBasicCommands {
-	
+public class SeleniumCommands2 {
+
 	WebDriver driver;
 	
 	@Test
-	public void seleniumCommands() throws InterruptedException {
+	public void seleniumCoreCommands() throws InterruptedException {
+		
 		
 		if (System.getProperty("os.name").equals("windows")) {
 			System.setProperty("webdriver.gecko.driver",
@@ -46,7 +47,7 @@ public class SeleniumBasicCommands {
 		assertEquals(loginPageTitle, "Login - My Store", "FAIL -- Login page title did not match");
 		
 		WebElement authenticationText = driver.findElement(By.xpath("//div[@id='center_column']/h1")); //Fetch the authentication web element
-		String loginText = authenticationText.getText(); //Get the text from the web elelemnt
+		String loginText = authenticationText.getText(); //Get the text from the web element
 		assertEquals(loginText, "AUTHENTICATION" , "FAIL -- Authentication text did not match");  //Compare the text with expected
 		
 		WebElement loginModule = driver.findElement(By.xpath("//form[@id='login_form']")); //Fetching login module web element
@@ -60,24 +61,42 @@ public class SeleniumBasicCommands {
 		
 		driver.findElement(By.xpath("//button[@id='SubmitLogin']")).click();  //Identifying the Submit button and clicking on it
 		
-		String myaccountTitle = driver.getTitle();
-		assertEquals(myaccountTitle, "My account - My Store", "FAIL -- My account title did not match");
+		driver.findElement(By.xpath("//a[@title='Information']/span")).click();  //Click the My personal information button
 		
-		String username = driver.findElement(By.xpath("//a[@class='account']/span")).getText();
-		System.out.println(username);
-		assertEquals(username, "Test Harsha", "FAIL -- Username did not match");
+		String infoText = driver.findElement(By.xpath("//div[@id='center_column']//h1")).getText(); //fetch the info text
+		assertEquals(infoText, "YOUR PERSONAL INFORMATION" ,"FAIL -- Info text did not match");
+		
+		WebElement dateDropDown = driver.findElement(By.xpath("//select[@id='days']")); //Fetch the date drop down web element
+		Select dateDD = new Select(dateDropDown);  //Put the web element into the select method
+		dateDD.selectByIndex(6);  //select the option using index (6)
+		Thread.sleep(4000);
+		
+		WebElement monthDropDown = driver.findElement(By.xpath("//select[@id='months']")); //Fetch the month drop down web element
+		Select monthDD = new Select(monthDropDown);  //Put the web element into the select method
+		monthDD.selectByValue("8");  //select the option using value (August)
+		Thread.sleep(4000);
+		
+		WebElement yearDropDown = driver.findElement(By.xpath("//select[@id='years']")); //Fetch the date drop down web element
+		Select yearDD = new Select(yearDropDown);  //Put the web element into the select method
+		yearDD.selectByVisibleText("1985  ");  //select the option using visible text (1985)
+		Thread.sleep(4000);
+		
+		driver.findElement(By.xpath("//input[@id='old_passwd']")).sendKeys("123456");  //enter old password
+		driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys("12345678");  //enter new password
+		driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("12345678");  //enter confirmation password
+		 
+		driver.findElement(By.xpath("//input[@id='newsletter']")).click();  //click on newsletter check box
+		driver.findElement(By.xpath("//input[@id='optin']")).click();  //click on special offers check box
+		
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[@name='submitIdentity']")).click();  //click on the save button
+		
 	}
-	
-	
-	
-	
 	
 	@AfterTest
 	public void endBrowser() {
-		//driver.close();   //This will close the current active browser window
-		driver.quit();    //This will quit the entire browser process from the backend
+		driver.quit();
 	}
 	
 	
-
 }
